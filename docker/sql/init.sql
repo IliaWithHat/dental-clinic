b539354f08725e88374609afe5fc0d34dc06b31d
IliaWithHat <rozhkoilia5533@gmail.com>
2024-06-09 10:25:04 +0300

    Add repository.

--- docker/sql/init.sql
+++ docker/sql/init.sql
@@ -7,3 +7,14 @@ CREATE TABLE working_time
     time_interval_in_minutes INT         NOT NULL,
     doctor_id                INT         NOT NULL
 );
+
+CREATE TABLE appointment
+(
+    id              SERIAL PRIMARY KEY,
+    date            TIMESTAMP NOT NULL,
+    is_patient_come BOOLEAN,
+    service_info    VARCHAR(256),
+    price           INT,
+    patient_id      INT       NOT NULL,
+    doctor_id       INT       NOT NULL
+);
