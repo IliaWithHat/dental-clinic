388f74a57d0a1d96c62ea6b23cc22c75ea145d4f
IliaWithHat <rozhkoilia5533@gmail.com>
2024-06-21 12:34:11 +0300

    Update bat file.

--- bat-files/stop.bat
+++ bat-files/stop.bat
@@ -1,9 +1,19 @@
 @echo off
 
-echo.
-echo Stopping Docker containers...
-docker stop postgres-container zookeeper-container kafka-container keycloak-container config-server-container eureka-server-container gateway-server-container user-service-container time-service-container appointment-service-container review-service-container mail-sender-service-container mail-scheduler-service-container
+if "%1"=="all" (
+    echo.
+    echo Stopping all Docker containers...
+    docker stop postgres-container zookeeper-container kafka-container keycloak-container config-server-container eureka-server-container gateway-server-container user-service-container time-service-container appointment-service-container review-service-container mail-sender-service-container mail-scheduler-service-container
 
-echo.
-echo Removing Docker containers...
-docker rm postgres-container zookeeper-container kafka-container keycloak-container config-server-container eureka-server-container gateway-server-container user-service-container time-service-container appointment-service-container review-service-container mail-sender-service-container mail-scheduler-service-container
+    echo.
+    echo Removing all Docker containers...
+    docker rm postgres-container zookeeper-container kafka-container keycloak-container config-server-container eureka-server-container gateway-server-container user-service-container time-service-container appointment-service-container review-service-container mail-sender-service-container mail-scheduler-service-container
+) else (
+    echo.
+    echo Stopping specific Docker containers...
+    docker stop config-server-container eureka-server-container gateway-server-container user-service-container time-service-container appointment-service-container review-service-container mail-sender-service-container mail-scheduler-service-container
+
+    echo.
+    echo Removing specific Docker containers...
+    docker rm config-server-container eureka-server-container gateway-server-container user-service-container time-service-container appointment-service-container review-service-container mail-sender-service-container mail-scheduler-service-container
+)
